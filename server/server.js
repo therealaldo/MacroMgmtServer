fcee9c8547d5c7f3f43b0629a4b66ff5b1462586
jacobnigh <jacobnigh@hotmail.com>
2016-07-18 15:06:57 -0400

    double semi colon cheese

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(body_parser.json());
 
 app.use('/', require('./routes/home.js')(express));
 app.use('/users', require('./routes/users.js')(express));
-app.use('/meals', require('./routes/meals.js')(express));;
+app.use('/meals', require('./routes/meals.js')(express));
 
 const server = app.listen(port, () => {
   console.log('Listening on port ' + port + '...');
