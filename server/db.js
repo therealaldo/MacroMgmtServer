769654048c0dc6a3c402061a57a8546375fd5696
therealaldo <agonzalez2@fullsail.edu>
2016-07-17 23:27:15 -0400

    Remove socket path

--- server/db.js
+++ server/db.js
@@ -9,7 +9,6 @@ module.exports = function() {
     host: process.env.DB_HOST,
     dialect: 'mysql',
     port: process.env.DB_PORT,
-    socketPath: '/var/run/mysqld/mysqld.sock',
     pool: {
       max: 5,
       min: 0,
