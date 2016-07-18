60e191a6d0d79c1d4c5959614a823c3187d8c4e4
jacobnigh <jacobnigh@hotmail.com>
2016-07-18 19:35:45 -0400

    still no clue

--- server/server.js
+++ server/server.js
@@ -1,21 +1,16 @@
 'use strict';
 
-// Config
 const express = require('express');
 const body_parser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Adding body parser to parse json automatically
 app.use(body_parser.json());
 
-// Routes
 app.use('/', require('./routes/home.js')(express));
 app.use('/users', require('./routes/users.js')(express));
 
-
-// Start server
 var server = app.listen(port, function() {
     console.log("Listening on " + port + "...");
 });
