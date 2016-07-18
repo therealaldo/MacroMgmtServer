f5457c924dea5c5bc30592176e3b714ca9ddd24c
jacobnigh <jacobnigh@hotmail.com>
2016-07-18 14:39:23 -0400

    routes were not included on server............

--- server/server.js
+++ server/server.js
@@ -9,6 +9,10 @@ const port = process.env.PORT || 3000;
 app.use(body_parser.json());
 
 app.use('/', require('./routes/home.js')(express));
+app.user('/users', require('./routes/users.js')(express));
+app.user('/meals', require('./routes/meals.js')(express));
+app.user('/trends', require('./routes/trends.js')(express));
+app.user('/intolerances', require('./routes/intolerances.js')(express));
 
 const server = app.listen(port, () => {
   console.log('Listening on port ' + port + '...');
