e3f69543b8e7e85888e16318faa7712331a34903
jacobnigh <jacobnigh@hotmail.com>
2016-07-18 19:47:50 -0400

    maybe fixed user route?

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,35 +1,33 @@
 'use strict';
-module.exports = function(express) {
-  const router = express.Router();
-  const async = require('async');
-  let users = require('../models/users.js');
-  const db = require('../db.js');
+module.exports = function (express) {
 
-  router.route('/users')
+const router = express.Router();
+const async = require('async');
+const db = require('../db.js');
+let users = require('../models/users.js');
 
-  //Put request to find a user if they are in the database otherwise if they are not create them
-  //and give back the user information needed within the app.
-  .put(function(req, res) {
-    //Setting data the the request.body
-    let data = req.body;
-    //Using async to do actions in order to receive the right information back.
-    async.waterfall([
-      users.findOrCreate({ where: { userId: data.userId }, defaults: { email: data.email, token: data.token}},
-      function(err) {
-        res.status(500).json({error: err});
-      }, function(user) {
-        callback(null, user);
-      })
-    ],
-    //Callback function to return the values from the fn() above.
-    function(err, user) {
-      if(err) {
-        res.status(500).json({ error: err });
-      } else{
-        res.status(200).json(user);
-      }
+router.route('/users')
+.put(function(req, res) {
+  let data = req.body;
+
+  async.waterfall([
+
+    users.findOrCreate({ where: {userId: data.userId },
+    defaults: { email: data.email, toke: data.token}
+    }, function(err) {
+      res.status(500).json({ error: err });
+    }, function(user) {
+      callback(null, user);
     })
+  ], function(err, user) {
+    if(err) {
+      res.status(500).json({ error: err });
+    } else {
+      res.status(200).json(user);
+    }
   })
+})
 
-  return router;
+return router;
+
 }
