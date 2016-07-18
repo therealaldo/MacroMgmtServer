8bb1671e7fbd9cba31cb27dab98a796df9922273
jacobnigh <jacobnigh@hotmail.com>
2016-07-18 19:34:55 -0400

    I literally have no idea what is wrong...

--- server/server.js
+++ server/server.js
@@ -1,18 +1,23 @@
 'use strict';
 
+// Config
 const express = require('express');
 const body_parser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Adding body parser to parse json automatically
 app.use(body_parser.json());
 
+// Routes
 app.use('/', require('./routes/home.js')(express));
 app.use('/users', require('./routes/users.js')(express));
 
-const server = app.listen(port, function() {
-  console.log('Listening on port ' + port + '...');
+
+// Start server
+var server = app.listen(port, function() {
+    console.log("Listening on " + port + "...");
 });
 
 module.exports = server;
