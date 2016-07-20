4fa564f0a1c0bf5c532ad77ef40444f3b4d86577
therealaldo <agonzalez2@fullsail.edu>
2016-07-20 03:56:54 -0400

    Changed the headers

--- server/server.js
+++ server/server.js
@@ -11,10 +11,7 @@ app.use(body_parser.json());
 app.use('/', require('./routes/home.js')(express));
 app.use('/users', require('./routes/users.js')(express));
 
-app.use((req, res, next) => {
-  res.header('X-powered-by', "Blood, sweat, and tears");
-  next();
-});
+app.set('X-powered-by', 'Blood, sweat, and tears');
 
 var server = app.listen(port, () => {
     console.log("Listening on " + port + "...");
