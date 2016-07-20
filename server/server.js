b9ebdbd36c14c7fa1fc2134a1a1f966f5d941e26
therealaldo <agonzalez2@fullsail.edu>
2016-07-20 05:55:51 -0400

    Trying to create http server

--- server/server.js
+++ server/server.js
@@ -1,20 +1,21 @@
 'use strict';
 
 const express = require('express');
+const http = require('http');
 const body_parser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(body_parser.json());
+app.use(body_parser.urlencoded({ extended: true }));
 
 app.use('/', require('./routes/home.js')(express));
 app.use('/users', require('./routes/users.js')(express));
 
 app.disable('x-powered-by');
 
-var server = app.listen(port, () => {
-    console.log("Listening on " + port + "...");
-});
+app.set('trust proxy', true);
+app.set('trust proxy', 'loopback');
 
-module.exports = server;
+http.createServer(app).listen(port || 3000);
