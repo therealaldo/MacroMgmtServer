da73a6a1f198d8229619b722ab62b2b9f34b4652
therealaldo <agonzalez2@fullsail.edu>
2016-07-20 09:11:05 -0400

    Fixed users route

--- routes/users.js
+++ routes/users.js
@@ -15,22 +15,13 @@ router.route('/')
 .put(function(req, res) {
   let data = req.body;
 
-  async.waterfall([
-
-    users.findOrCreate({ where: {userId: data.userId },
-    defaults: { email: data.email, token: data.token}
-    }, function(err) {
-      res.status(500).json({ error: err });
-    }, function(user) {
-      callback(null, user);
-    })
-  ], function(err, user) {
-    if(err) {
-      res.status(500).json({ error: err });
-    } else {
-      res.status(200).json(user);
-    }
-  });
+  users.findOrCreate({ where: {userId: data.userId },
+  defaults: { email: data.email, token: data.token}
+  }, function(err) {
+    res.status(500).json({ error: err });
+  }, function(user) {
+    callback(null, user);
+  })
 });
 
 return router;
