ccc0671c9495767e986c81e7346042042c50144d
therealaldo <agonzalez2@fullsail.edu>
2016-07-25 02:39:02 -0400

    removed the custom primary key from the user meals

--- server/db.js
+++ server/db.js
@@ -43,11 +43,6 @@ module.exports = function() {
   });
 
   const _userMeals = _sequelize.define('userMeals', {
-    id: {
-      type: Sequelize.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
     date: {
       type: Sequelize.STRING
     },
