67193c9617985c6740fe8dea388d8fe1a2919d68
jacobnigh <jacobnigh@hotmail.com>
2016-07-25 05:37:37 -0400

    setting up basic code layout for other routes, also added the grocery list route

--- routes/trends.js
+++ routes/trends.js
@@ -1,1 +1,18 @@
-//For charts
+'use strict';
+module.exports = function(express) {
+  const router = express.Router();
+  const async = require('async');
+  let trends = require('../models/grocery_lists.js');
+  let meals = require('../models/meals.js');
+  let users = require('../models/users.js');
+  const db = require('../server/db.js');
+
+  router.route('/')
+
+  .get(function(req, res) {
+
+  })
+
+
+  return router;
+};
