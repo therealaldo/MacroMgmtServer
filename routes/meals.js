dadb401cc1ead81eac99be98d3e484113c545101
therealaldo <agonzalez2@fullsail.edu>
2016-07-25 03:14:31 -0400

    trying to add meal to user

--- routes/meals.js
+++ routes/meals.js
@@ -78,8 +78,8 @@ module.exports = function(express) {
               image: data.meal.image
             }
           }).then((meal) => {
-            let meal1 = meal[0];
-            res.status(200).json({ meal1 });
+            let foundOrNewMeal = meal[0];
+            res.status(200).json({ user });
           }).catch((err) => {
             res.status(500).json({ error: err });
           })
