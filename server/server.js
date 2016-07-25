006352ff6590a2cf26bfb25fdc69a8737609fffd
therealaldo <agonzalez2@fullsail.edu>
2016-07-25 13:00:41 -0400

    the error went away when removing both routes

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use('/', require('../routes/home.js')(express));
 app.use('/users', require('../routes/users.js')(express));
 app.use('/meals', require('../routes/meals.js')(express));
 app.use('/grocerylist', require('../routes/grocery_list.js')(express));
-
+app.use('/ingredients', require('../routes/ingredients.js')(express));
 app.use('/trends', require('../routes/trends.js')(express));
 
 app.disable('x-powered-by');
